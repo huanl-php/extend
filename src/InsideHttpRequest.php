ca58892efd875360d29fccc82c99c65c8a8e85a5
CodFrm <code.farmer@qq.com>
2018-06-15 10:34:46 +0800

    简单的模拟请求

--- src/InsideHttpRequest.php
+++ src/InsideHttpRequest.php
@@ -16,16 +16,121 @@ class InsideHttpRequest {
      */
     protected $start_file = '';
 
-    public function __construct(string $start_file) {
+    public function __construct(string $start_file = '') {
         $this->start_file = $start_file;
+        $_SERVER['SCRIPT_NAME'] = '';
     }
 
-    public function get() {
-
+    /**
+     * get请求
+     * @param string $uri
+     * @param array $param
+     * @return InsideHttpRequest
+     */
+    public function get($uri = '/', $param = []) {
+        return $this->method('GET', $uri, $param);
     }
 
-    public function method($method, $uri) {
-        $_SERVER['HTTP_ACCEPT']='POST';
+    /**
+     * post请求
+     * @param string $uri
+     * @param array $param
+     * @return InsideHttpRequest
+     */
+    public function post($uri = '/', $param = []) {
+        return $this->method('POST', $uri, $param);
+    }
 
+    /**
+     * put请求
+     * @param string $uri
+     * @param array $param
+     * @return InsideHttpRequest
+     */
+    public function put($uri = '/', $param = []) {
+        return $this->method('PUT', $uri, $param);
+    }
+
+    /**
+     * delete请求
+     * @param string $uri
+     * @param array $param
+     * @return InsideHttpRequest
+     */
+    public function delete($uri = '/', $param = []) {
+        return $this->method('DELETE', $uri, $param);
+    }
+
+    /**
+     * 自定义请求方法和uri
+     * @param $method
+     * @param string $uri
+     * @return $this
+     */
+    public function method($method, $uri = '/', $param = []) {
+        $_SERVER['REQUEST_METHOD'] = $method;
+        $_SERVER['REQUEST_URI'] = $uri;
+        if (is_array($param)) {
+            $_GET = $param;
+            $queryString = '';
+            foreach ($param as $key => $value) {
+                $queryString .= "$key=$value&";
+            }
+            $_SERVER['QUERY_STRING'] = $queryString;
+        }
+        return $this;
+    }
+
+    /**
+     * http
+     * @return $this
+     */
+    public function http() {
+        $_SERVER['REQUEST_SCHEME'] = 'http';
+        return $this;
+    }
+
+    /**
+     * https
+     * @return $this
+     */
+    public function https() {
+        $_SERVER['REQUEST_SCHEME'] = 'https';
+        return $this;
+    }
+
+    /**
+     * 域名
+     * @param string $domain
+     * @return $this
+     */
+    public function domain(string $domain) {
+        $_SERVER['SERVER_NAME'] = $domain;
+        return $this;
+    }
+
+    /**
+     * 设置请求数据
+     * @param $data
+     * @return $this
+     */
+    public function data($data) {
+        $dataString = '';
+        if (is_array($data)) {
+            $_POST = $data;
+            foreach ($data as $key => $value) {
+                $dataString .= "$key=$value&";
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * 发送请求
+     * @return $this
+     */
+    public function request() {
+        require_once $this->start_file;
+        return $this;
     }
 }
